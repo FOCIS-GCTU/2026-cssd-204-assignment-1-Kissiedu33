8ed90ddef8f633fb9a8e0ed30956f281ef01ebfa
Kissiedu33 <2425403199@live.gctu.edu.gh>
2026-05-30 17:46:40 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+# File: <initials.py>
+# Description: <A code to orint the intials of my name in block letters>
+# Assignment Number: <Assignment Number 1 >
+#
+# Name: <Kissiedu Theophilus Ofori>
+# STUDENT ID:  <2425403199>
+# Email: <kissiedut@gmail.com>
+# Grader: <Augustus Buckman>
+#
+# On my honor, <Kissiedu Theophilus Ofori>, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
 
 
+def main():     #this is the main function that will be called when the program runs.
+
+    print()         #this is a blank line to make the output look nicer
+
+    print("...KTO")     #this is the first line of the output.
+    print()   #this is a blank line to make the output look nicer
+
+#the following lines are the rest of the output. Each line is printed separately to make it easier to read and understand.
+    print("...KKKK....KKKK........TTTTTTTTTTTT........OOOOOOOOOOOO...")
+    print("...KKKK....KKKK........TTTTTTTTTTTT........OOO......OOO...")
+    print("...KKKK....KKKK...........TTTTT............OOO......OOO...")
+    print("...KKKK...KKKK............TTTTT............OOO......OOO...")
+    print("...KKKK..KKKK.............TTTTT............OOO......OOO...")
+    print("...KKKK.KKKK..............TTTTT............OOO......OOO...")
+    print("...KKKK..KKKK.............TTTTT............OOO......OOO...")
+    print("...KKKK...KKKK............TTTTT............OOO......OOO...")
+    print("...KKKK....KKKK...**......TTTTT.......**...OOO......OOO...")
+    print("...KKKK....KKKK...**......TTTTT.......**...OOOOOOOOOOOO...")
+
+    print()     #this is a blank line to make the output look nicer
 main()
+
+
+
